fe5d437e20211b2a61f85dad98da233492e194ac
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-05-23 22:11:39 +0100

    Update:

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,15 +1,9 @@
 import React from "react"
 
-import { useTheme } from "@material-ui/core/styles"
-import useMediaQuery from "@material-ui/core/useMediaQuery"
-
 import Box from "@material-ui/core/Box"
 import { Container, Link } from "@material-ui/core"
 
 export default function TopNavLinks() {
-  const theme = useTheme()
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"))
-
   return (
     <Box width={1}>
       <Container>
@@ -17,7 +11,7 @@ export default function TopNavLinks() {
           width={1}
           alignItems="center"
           justifyContent="around"
-          display={isMobile ? "block" : "flex"}>
+          display={{ xs: "block", md: "flex" }}>
           <Box pt={2} width={{ sm: "1", md: "1 / 2" }}>
             <Box fontSize="h3.fontSize" fontWeight="fontWeightBold">
               Amazing
@@ -30,7 +24,10 @@ export default function TopNavLinks() {
             </Box>
           </Box>
           <Box pt={2} width={{ sm: "1", md: "1 / 2" }}>
-            <img src="/img/hero-1.png" style={{ maxWidth: "100%" }} />
+            <img
+              src="/img/hero-1.png"
+              style={{ maxWidth: "100%", margin: "auto 0" }}
+            />
           </Box>
         </Box>
       </Container>
